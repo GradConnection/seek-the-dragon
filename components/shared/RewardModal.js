b28644761a9fa4a267bc4863fad234b97a8fe711
CaitlynFowkes <caitlyn@gradconnection.com>
2021-11-18 00:27:20 +0200

    Add reward modal and dragons on level up

--- components/shared/RewardModal.js
+++ components/shared/RewardModal.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import animatedStar from '../../public/star_animated.gif'
+import Dragon from '../../components/shared/Dragon'
 
-const RewardModal = ({levelUpImage = animatedStar, showModal = true }) => {
-const [show, setShow] = useState(showModal)
-if (!show) {
+const RewardModal = ({levelUpImage = animatedStar, showModal, setShowModal, modalTitle = 'Well done!', 
+    modalDescription = "You're on your way to levelling up. Keep up the good work.", showDragon}) => {
+if (!showModal) {
     return null;
 }
  return (
@@ -13,13 +14,14 @@ if (!show) {
     <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
  <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
    <div className="">
-     <div className="text-center p-5 flex-auto justify-center">
-     <Image
+     <div className="text-center p-5 justify-center">
+       {showDragon ? <Dragon width='150px' height='150px'/>  :
+        <Image
             src={levelUpImage}
             alt={levelUpImage}
-        />
-        <h2 className="text-xl font-bold py-4 ">Well done!</h2>
-        <p className="text-sm text-gray-500 px-8">You're on your way to levelling up. Keep up the good work.</p>    
+        />}
+        <h2 className="text-xl font-bold py-4 ">{modalTitle}</h2>
+        <p className="text-sm text-gray-500 px-8">{ modalDescription }</p>    
      </div>
   
      <div className="p-3  mt-2 text-center space-x-4 md:block">
@@ -28,8 +30,7 @@ if (!show) {
              Go to profile
          </button>
     </Link>
-         <button onClick={() => {setShow(false)}} className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Continue</button>
-         {/* <button onClick={() => {confetti();}} className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Continue</button> */}
+         <button onClick={() => {setShowModal(false)}} className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Continue</button>
      </div>
    </div>
  </div>
