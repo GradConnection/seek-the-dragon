5d38f6d9581f0a01548a81d59cd0751e5e635d1f
William-Francis <William-Francis@users.noreply.github.com>
2021-11-17 11:16:13 +0200

    added interactive map

--- pages/map.js
+++ pages/map.js
@@ -2,61 +2,88 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import React, { useEffect } from "react";
 
 function Map() {
-  function drawCircle(canvas) {
-    const context = canvas.getContext("2d");
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const radius = 70;
-
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = "green";
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = "#003300";
-    context.stroke();
-  }
   useEffect(() => {
+    // Make circle
+    function makeCircle(context, name, x, y, color, circleSize) {
+      context.font = "30px Arial";
+      context.fillStyle = "white";
+      context.fillText(name, x - circleSize, y - circleSize - 10);
+      context.beginPath();
+      context.lineWidth = 5;
+
+      context.arc(x, y, circleSize, 0, Math.PI * 2, true);
+      // context.fillStyle = color;
+      context.stroke();
+      // context.fill();
+      context.closePath();
+    }
+
     const canvas = document.getElementById("canvas");
+    // c.width = window.innerWidth;
+    // c.height = window.innerHeight;
     let context = canvas.getContext("2d");
-    // drawCircle(canvas)
     canvas.width = 1900;
     canvas.height = 1400;
-    context.beginPath();
-    context.lineWidth = 5;
-    context.arc(1000, 700, 200, 0, 2 * Math.PI);
-    context.stroke();
 
-    // var background = new Image();
-    // background.src = "https://static.vecteezy.com/system/resources/previews/003/758/266/original/fairytale-kingdom-map-medieval-village-background-fairy-tale-castle-infographic-elements-with-sea-mountains-forest-ship-fantasy-town-plan-with-unicorn-witch-mermaid-dragon-frog-prince-vector.jpg";
+    var elements = [];
+    elements.push({
+      name: "Forest",
+      color: "#05EFFF",
+      circleSize: 50,
+      x: 200,
+      y: 200,
+      clicked: function () {
+        alert("This Forest circle has been clicked");
+      },
+    });
+    elements.push({
+      name: "Castle",
+      color: "#05EFFF",
+      circleSize: 50,
+      x: 650,
+      y: 550,
+      clicked: function () {
+        alert("This Castle circle has been clicked");
+      },
+    });
+    elements.forEach(function (element) {
+      var circle = new makeCircle(
+        context,
+        element.name,
+        element.x,
+        element.y,
+        element.color,
+        element.circleSize
+      );
+      console.log(circle);
+    });
 
-    // background.onload = function(){
-    //     context.drawImage(background,0,0);
-    // }
-    //      const centerX = canvas.width / 2;
-    //     const centerY = canvas.height / 2;
-    //     const radius = 120;
+    canvas.addEventListener("click", function (e) {
+      var x = e.pageX - canvas.offsetLeft;
+      var y = e.pageY - canvas.offsetTop;
 
-    //     context.beginPath();
-    //     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    //     context.fillStyle = 'green';
-    //     context.fill();
-    //     context.lineWidth = 5;
-    //     context.strokeStyle = '#003300';
-    //     context.stroke();
-    // canvas.width = 1900;
-    // canvas.height = 1400;
+      elements.forEach(function (element) {
+        if (
+          Math.pow(x - element.x, 2) + Math.pow(y - element.y, 2) <
+          Math.pow(element.circleSize, 2)
+        ) {
+          element.clicked();
+        }
+      });
+    });
   }, []);
 
   return (
     <div>
       <img
-        style={{ height: 900, left: 0 }}
+        style={{ height: 900, left: 0, top: 0 }}
         src="https://static.vecteezy.com/system/resources/previews/003/758/266/original/fairytale-kingdom-map-medieval-village-background-fairy-tale-castle-infographic-elements-with-sea-mountains-forest-ship-fantasy-town-plan-with-unicorn-witch-mermaid-dragon-frog-prince-vector.jpg"
         alt="Map"
       />
-      <canvas style={{ position: "absolute", left: 0 }} id="canvas"></canvas>
-      {/* {{canvas}} */}
+      <canvas
+        style={{ position: "absolute", left: 0, top: 0 }}
+        id="canvas"
+      ></canvas>
     </div>
   );
 }
