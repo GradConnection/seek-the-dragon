3a4ae8d81131b596a8d9d2e1861ed792fff1fa7f
dgorobets <dmitra90@gmail.com>
2021-11-17 18:17:20 +0500

    Make the text field bigger

--- pages/congratulations.js
+++ pages/congratulations.js
@@ -41,8 +41,7 @@ export default function Congratulations() {
                     <input
                         onChange={onChange}
                         placeholder="Amazing company"
-                        id="employer_input"
-                        className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-2"
+                        className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-lg text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-2"
                         type="text"
                         value={companyName}
                     />
